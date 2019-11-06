df2b1b88a7d7cec1bbc3c945f50412dd2f18475f
whispyy <jfdurk@gmx.fr>
2019-11-05 22:07:38 -0500

    feat(Send): Creates Fab button + place it

--- src/shared/themes/Images.ts
+++ src/shared/themes/Images.ts
@@ -2,6 +2,7 @@ import backIcon from './assets/back-icon.png'
 import menuIcon from './assets/menu-icon24.png';
 import copyIcon from './assets/copy-icon24.png';
 import moreIcon from './assets/more-icon24.png';
+import planeIcon from './assets/paper-plane24.png';
 
 import bitcoinIcon from './assets/crypto/bitcoin.png';
 
@@ -11,6 +12,7 @@ export default {
   menuIcon,
   copyIcon,
   moreIcon,
+  planeIcon,
 
   //crypto iconts
   bitcoinIcon,
